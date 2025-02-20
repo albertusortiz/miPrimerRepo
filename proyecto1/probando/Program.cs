b54ede8ec3a011f1c494ea2bdac5eaba2e475c33
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2025-02-19 22:28:19 -0600

    Agregando test string connection

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -2,3 +2,5 @@
 Console.WriteLine("Hello, Platzi!");
 
 string stripe_api_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc";
+
+string testingConnection = "Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;";
